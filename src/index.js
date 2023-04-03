9209bc806008be32acbb3514b87d082cf0173c36
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-03 18:15:24 +0300

    feat: created the basic components of the application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/App/App'
 
-createRoot(document.getElementById('root')).render(<App />)
+import './styles/index.css'
+
+createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
