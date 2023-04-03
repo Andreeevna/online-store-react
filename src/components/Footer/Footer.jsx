ccd5bcb58e070ddd7e58fc93c8967ba5a2edff5c
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-03 19:44:51 +0300

    style: footer implemented

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import LOGO from '../../images/logo.svg'
+import styles from '../../styles/Footer.module.css'
+import { ROUTES } from '../../utils/routes'
+
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<section className={styles.footer}>
+			<div className={styles.logo}>
+				<Link to={ROUTES.HOME}>
+					<img src={LOGO} alt='stuff' />
+				</Link>
+			</div>
+
+			<div className={styles.rights}>Developed by Andreeevna</div>
+
+			<div className={styles.socials}>
+				<a href='#' target='_blank' rel='noreferrer'>
+					<svg className='icon'>
+						<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+					</svg>
+				</a>
+				<a href='#' target='_blank' rel='noreferrer'>
+					<svg className='icon'>
+						<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+					</svg>
+				</a>
+				<a href='#' target='_blank' rel='noreferrer'>
+					<svg className='icon'>
+						<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+					</svg>
+				</a>
+			</div>
+		</section>
+	)
 }
 
 export default Footer
