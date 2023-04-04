1480e3f78742a3fef596a28961b2ce801910b4c4
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-04 17:19:57 +0300

    feat: roll up the poster component

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import styles from '../../styles/Home.module.css'
+
+import BG from '../../images/computer.png'
+
 const Poster = () => {
-	return <div>Poster</div>
+	return (
+		<section className={styles.home}>
+			<div className={styles.home}>BIG SALE 20%</div>
+			<div className={styles.product}>
+				<div className={styles.text}>
+					<div className={styles.subtitle}>the bestseller of 2022 </div>
+					<h1 className={styles.head}>LENNON r2d2 with NVIDIA 5090 TI</h1>
+					<button className={styles.button}>Shop Now</button>
+				</div>
+				<div className={styles.image}>
+					<img src={BG} alt='background' />
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Poster
