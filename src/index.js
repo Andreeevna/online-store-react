c444b99ce283fe5b6662f9c81221edb1576eb629
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-12 21:00:46 +0300

    test4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import { store } from './redux/store'
 
@@ -10,8 +10,8 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 )
