8a9d7b85f36ae180fc210096b54cad94236a3a7a
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-12 19:11:54 +0300

    feat: added the cart component to the routes

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { ROUTES } from '../../utils/routes'
 
+import Cart from '../Cart/Cart'
 import SingleCategory from '../Categories/SingleCategory'
 import Home from '../Home/Home'
 import SinglProduct from '../Products/SinglProduct'
@@ -15,6 +16,7 @@ const AppRoutes = () => {
 			<Route path={ROUTES.PRODUCTS} element={<SinglProduct />} />
 			<Route path={ROUTES.PROFILE} element={<Profile />} />
 			<Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
+			<Route path={ROUTES.CART} element={<Cart />} />
 		</Routes>
 	)
 }
