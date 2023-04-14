42abe0b1df6231e7e34dfd04657fffd5dda3e0ef
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-14 21:24:15 +0300

    fix: fix: fixed a bug with the public folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { store } from './redux/store'
 
@@ -10,8 +10,8 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<HashRouter basename={window.location.pathname || ''}>
+		<BrowserRouter>
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	</Provider>
 )
